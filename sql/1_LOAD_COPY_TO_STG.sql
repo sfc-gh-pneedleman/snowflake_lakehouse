e9d4e255bdfc41fb491492ab94fd7cbe894e5e95
Paul Needleman <89047014+sfc-gh-pneedleman@users.noreply.github.com>
2022-07-26 09:42:44 -0400

    Update 1_LOAD_COPY_TO_STG.sql

--- sql/1_LOAD_COPY_TO_STG.sql
+++ sql/1_LOAD_COPY_TO_STG.sql
@@ -8,21 +8,27 @@ CREATE STAGE JSON_DATA_STAGE;
 CREATE STAGE PDF_FILE_STAGE
 directory = (enable = true )
 ENCRYPTION = (TYPE =  'SNOWFLAKE_SSE');
-/* Load data files into the newly created Snowflake stages */
---upload customer data. Not this command should be run via SnoqSQL 
 
--- #### RUN the below command via SnowSQL to uplaod data to your Stage ####
---put file://json/customer_data/* @json_data_stage/customer_data/;
--- ####                                                     
+-- Load data files into the newly created Snowflake stages 
+
+-- #####
+-- ## upload customer data. Not this command should be run via SnoqSQL 
+-- ## RUN the below command via SnowSQL to uplaod data to your Stage.
+-- ## Relative path to execute this command is from ./files/
+-- ######
+put file://json/customer_data/* @json_data_stage/customer_data/;                                                 
 
 
---note this command to upload PDFs will take about 10min for me given that 1GB of data going through my upload speed 
--- ##### RUN the below command via SnowSQL to uplaod data to your Stage #####
---put file://pdfs/* @pdf_file_stage AUTO_COMPRESS=false;
---#######
+-- ##### 
+-- ## note this command to upload PDFs will take about 10min for me given that 1GB of data going through my upload speed 
+-- ## RUN the below command via SnowSQL to uplaod data to your Stage 
+-- ## Relative path to execute this command is from ./files/
+-- ##### 
+put file://pdfs/* @pdf_file_stage AUTO_COMPRESS=false;
 
 --since this is a directory stage used to support unstructured data, we must refresh the directory structure 
 ALTER STAGE PDF_FILE_STAGE REFRESH;
-/* lets review our objects and take a quick look at the stages */
+
+--lets review our objects and take a quick look at the stages
 LIST @JSON_DATA_STAGE;
 LIST @PDF_FILE_STAGE;
