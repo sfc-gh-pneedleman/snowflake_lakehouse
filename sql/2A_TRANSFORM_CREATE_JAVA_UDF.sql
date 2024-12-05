67dbb0c1968303746797f7a33ebc9b939c184573
Paul Needleman <89047014+sfc-gh-pneedleman@users.noreply.github.com>
2024-12-04 19:56:19 -0500

    added option for python or java UDF

--- sql/2A_TRANSFORM_CREATE_JAVA_UDF.sql
+++ sql/2A_TRANSFORM_CREATE_JAVA_UDF.sql
@@ -39,3 +39,26 @@ return null;
 }
 $$;
 
+--Can also use Python UDF
+CREATE FUNCTION IF NOT EXISTS pdf_to_text_udf(file_path string)
+RETURNS VARCHAR
+LANGUAGE PYTHON
+RUNTIME_VERSION = '3.8'
+PACKAGES = ('snowflake-snowpark-python', 'pypdf2')
+HANDLER = 'main'
+AS
+$$
+from snowflake.snowpark.files import SnowflakeFile
+from PyPDF2 import PdfFileReader
+from io import BytesIO
+ 
+def main(file_path):
+    with SnowflakeFile.open(file_path, 'rb') as f:
+        f = BytesIO(f.readall())
+        reader = PdfFileReader(f)
+        pageObj = reader.pages[0]
+        text = pageObj.extractText()
+        return text
+$$;
+
+
